4654935399ce63330364864aef830a465c410fe9
michi-281255 <168687215+michi-281255@users.noreply.github.com>
2024-06-02 03:00:14 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const Btn = document.getElementById('btn');
+
+Btn.addEventListener('click',() => {
+  const Text = document.getElementById('text');
+  setTimeout(() => {
+    Text.textContent= 'ボタンをクリックしました';
+  },2000);
+});
